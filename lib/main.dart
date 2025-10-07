9ec30272387a9129c6caf3d20730af51261145dd
Atifeen <atifynezubayer@gmail.com>
2025-10-07 18:38:46 +0600

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_shop/auth/login_or_register.dart';
+import 'package:grocery_shop/themes/dark_mode.dart';
 import 'package:grocery_shop/themes/light_mode.dart';
 
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: lightMode,
+      theme: darkMode,
       home: const LoginOrRegister(),
     );
   }
