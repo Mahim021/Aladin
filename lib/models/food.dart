0bf1d41fba9eca296f5d1e8f3db4be91d7a8fdcc
Md. Aiful Alam Mahim <arifulalam7865@gmail.com>
2025-10-10 22:56:23 +0600

    Added Food and Restaurant models(dart files only)

---
+++ lib/models/food.dart
@@ -0,0 +1,36 @@
+class Food{
+  final String name;
+  final String imagePath;
+  final String description;
+  final double price;
+  final FoodCategory category;
+  final List<Addon> addons;
+ 
+
+  Food({
+    required this.name,
+    required this.imagePath,
+    required this.description,
+    required this.price,
+    required this.category,
+    required this.addons,
+  });
+}
+
+enum FoodCategory {
+  burger,
+  salads,
+  sides,
+  desserts,
+  drinks,
+}
+
+class Addon{
+  final String name;
+  final double price;
+
+  Addon({
+    required this.name,
+    required this.price,
+  });
+}
